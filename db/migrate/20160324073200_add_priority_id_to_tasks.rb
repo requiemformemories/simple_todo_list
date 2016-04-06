82226a244b0409198053024f9b24702eba872bbd
文月文月吃八盤(?) <requiem.for.memories@gmail.com>
2016-04-06 09:15:24 +0000

    fix migrations for Heroku

--- db/migrate/20160324073200_add_priority_id_to_tasks.rb
+++ db/migrate/20160324073200_add_priority_id_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddPriorityIdToTasks < ActiveRecord::Migration
   def change
-    add_column :tasks, :priority_id, :text
+    
   end
 end
--- db/migrate/20160324073358_change_priority_id.rb
+++ db/migrate/20160324073358_change_priority_id.rb
@@ -1,6 +1,6 @@
 class ChangePriorityId < ActiveRecord::Migration
   def change
-    change_column :tasks, :priority_id, :integer
+    add_column :tasks, :priority_id, :integer
     add_index :tasks, :priority_id
   end
 end
